871e04b2f1c4ea7c376983435b7d70f1d8785b80
kiryauragan <you@example.comkirilltaranskyi@gmail.com>
2023-06-23 20:56:44 +0300

    project final

---
+++ myreact1/src/components/LeftNav.js
@@ -0,0 +1,11 @@
+export default function LeftNav() {
+	return (
+		<ul>
+			<nav><a href="#">link</a></nav>
+			<nav><a href="#">link</a></nav>
+			<nav><a href="#">link</a></nav>
+			<nav><a href="#">link</a></nav>
+			<nav><a href="#">link</a></nav>
+		</ul>
+	)
+}
